61d9453c8cdca2f4ae94490e6304284dc28e070a
Darren Murray <dmurray@ccistudios.com>
2011-06-14 17:13:17 -0400

    added various structural items

--- template/templates/fritopies/index.php
+++ template/templates/fritopies/index.php
@@ -13,32 +13,35 @@ if ($menu)
 	<link rel="stylesheet" href="/templates/fritopies/css/template.css" />
 </head>
 
-<body>
+<body class="<?php echo $menu; ?>">
 	<div id="top"><div>
-		<jdoc:include type="modules" name="mainmenu" style="xhtml">
-		<div class="clear"></div>
+		<div id="mainmenu">
+			<jdoc:include type="modules" name="main_menu" style="xhtml" />
+			<div class="clear"></div>
+		</div>
 	</div></div>
 	
 	<div id="content">
-		<div id="component">
-			
-		</div>	
-		<div id="bottom">
+		<div id="component"><div>
+			<jdoc:include type="component">
+			<jdoc:include type="modules" name="bottom" style="xhtml" />
+			<div class="clear"></div>
+		</div></div>
 		
-		</div>
-	</div>
-		
-</body>
-	
-	<div id="footer"><div>
-		<div class="left">&copy; <?php echo date ('Y')?> Frito Pies & More. All Rights Reserved.</div>
-		<div class="right">Site by <a href="http://www.ccistudios.com" target="_blank">CCI Studios</a></div>
-	</div></div>		
+		<div id="bottom"><div>
+			<jdoc:include type="modules" name="bottom_menu" style="xhtml" />
+		</div></div>	
 
+		<div class="clear"></div>
+		<div id="footer"><div>
+			<div class="left">&copy; <?php echo date ('Y')?> Frito Pies & More. All Rights Reserved.</div>
+			<div class="right">Site by <a href="http://www.ccistudios.com" target="_blank">CCI Studios</a></div>
+		</div></div>		
+	</div>
+	
 	<div class="hidden">
 		<jdoc:include type="modules" name="hidden" style="xhtml" />
 		<div class="clear"></div>
-	</div>
-	
-
+	</div>	
+</body>
 </html>	
