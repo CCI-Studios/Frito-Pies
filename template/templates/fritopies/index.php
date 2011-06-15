d62897bb299d259a66ae51a785e70551045fd33d
Darren Murray <dmurray@ccistudios.com>
2011-06-15 17:04:53 -0400

    Corrected id placement within the site

--- template/templates/fritopies/index.php
+++ template/templates/fritopies/index.php
@@ -14,30 +14,43 @@ if ($menu)
 </head>
 
 <body class="<?php echo $menu; ?>">
-	<div id="top"><div>
-		<div id="mainmenu">
-			<jdoc:include type="modules" name="main_menu" style="xhtml" />
-			<div class="clear"></div>
-		</div>
+
+	<div id="mainmenu"><div>
+		<jdoc:include type="modules" name="main_menu" style="xhtml" />
+		<div class="clear"></div>
 	</div></div>
 	
-	<div id="content">
-		<div id="component"><div>
-			<jdoc:include type="component">
+	<div id="header"><div><div>
+		<div id="napkins"><img src="templates/fritopies/images/napkins.png" alt=""/></div>
+	</div></div></div>
+	
+	<div id="body"><div><div>
+		<div id="content">
+			<div id="component"><div>
+				<jdoc:include type="component" />
+			</div></div>
+		
+			<?php if ($this->countModules('sidebar')): ?>
+			<div id="sidebar">
+				<jdoc:include type="modules" name="sidebar" style="xhtml" />
+			</div>
+			<?php endif; ?>
+		
+			<div class="clear"></div>
+		</div>
+		
+		<div id="bottom">
 			<jdoc:include type="modules" name="bottom" style="xhtml" />
 			<div class="clear"></div>
-		</div></div>
-		
-		<div id="bottom"><div>
 			<jdoc:include type="modules" name="bottom_menu" style="xhtml" />
-		</div></div>	
-
-		<div class="clear"></div>
-		<div id="footer"><div>
-			<div class="left">&copy; <?php echo date ('Y')?> Frito Pies & More. All Rights Reserved.</div>
-			<div class="right">Site by <a href="http://www.ccistudios.com" target="_blank">CCI Studios</a></div>
-		</div></div>		
-	</div>
+		</div>
+	</div></div></div>
+	
+	<div id="footer"><div>
+		<div class="left">&copy; <?php echo date ('Y')?> Frito Pies & More. All Rights Reserved.</div>
+		<div class="right">Site by <a href="http://www.ccistudios.com" target="_blank">CCI Studios</a></div>
+	</div></div>	
+	
 	
 	<div class="hidden">
 		<jdoc:include type="modules" name="hidden" style="xhtml" />
