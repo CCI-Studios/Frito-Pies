7ab3d9b91db178c8de79985b4086c18dae767af3
Darren Murray <dmurray@ccistudios.com>
2011-06-20 12:37:00 -0400

    added clear

--- template/templates/fritopies/index.php
+++ template/templates/fritopies/index.php
@@ -73,13 +73,14 @@ if ($menu)
 			<jdoc:include type="modules" name="bottom" style="xhtml" />
 			<div class="clear"></div>
 			<jdoc:include type="modules" name="bottom_menu" style="xhtml" />
-		</div>
+		</div><div class="clear"></div>
 	</div></div></div>
 	
 	<div id="footer"><div>
 		<div class="left">&copy; <?php echo date ('Y')?> Frito Pies & More. All Rights Reserved.</div>
 		<div class="right">Site by <a href="http://www.ccistudios.com" target="_blank">CCI Studios</a></div>
 	</div></div>	
+	
 	
 	<div class="hidden">
 		<jdoc:include type="modules" name="hidden" style="xhtml" />
